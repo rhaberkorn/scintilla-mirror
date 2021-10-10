e04f4e9a885a832ff13906cb1fd1e9b8abfd1922
Robin Haberkorn <robin.haberkorn@googlemail.com>
2021-10-10 17:47:45 +0300

    commented out Editor::SetRepresentations()
    * This sets the default key representations on every SCI_SETDOCPOINTER
      which is very inefficient considering the way that SciTECO uses the
      Q-Register view.
      Furthermore it meant, we had to reset the representations to their
      SciTECO versions again after every SCI_SETDOCPOINTER.
    * This patch only does not cause problems because we initialize
      the representations anyway for every Scintilla view.
      This patch is not meant to be upstreamed!

--- src/Editor.cxx
+++ src/Editor.cxx
@@ -219,7 +219,7 @@ void Editor::Finalise() {
 }
 
 void Editor::SetRepresentations() {
-	reprs->SetDefaultRepresentations(pdoc->dbcsCodePage);
+//	reprs->SetDefaultRepresentations(pdoc->dbcsCodePage);
 }
 
 void Editor::DropGraphics() noexcept {
